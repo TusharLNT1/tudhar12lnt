ce816f8a5efef0fa1acb5d525444d02b0641be37
TusharLNT1 <120655185+TusharLNT1@users.noreply.github.com>
2023-05-02 03:37:12 +0530

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,40 @@
+
+<?php 
+
+session_start();
+
+
+?>
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style1.css">
+</head>
+<body>
+
+
+<form action="operation.php" method="POST">
+<div class="container">
+    <div class="row">
+      <h2 style="text-align:center">logout</h2>
+      
+        <div>
+        <button type="submit" name="operation" value="Logout" class="btn btn-primary" >Logout</button>
+      </div>
+      
+    </div>
+  
+</div>
+
+
+    
+  </div>
+</div>
+</form>
+</body>
+</html>
